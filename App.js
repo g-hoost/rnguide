a0b1757e6b8b144cf154aba65769bd48b35ed8a0
Georgia Hoost <gfroehling@icloud.com>
2021-08-18 18:01:28 +0200

    Initial

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function App()
+{
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>RNGuide with modal and stuff!</Text>
+      <Button title="ADD" />
       <StatusBar style="auto" />
     </View>
   );
