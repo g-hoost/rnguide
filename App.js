34ddaa8dbb6d2742fea948fdbd5fec7314af6df4
g-hoost <gfroehling@icloud.com>
2021-08-25 17:39:32 +0200

    stylesheet done

--- App.js
+++ App.js
@@ -6,24 +6,27 @@ export default function App()
 {
   return (
     <View style={styles.container}>
-      <View>
+      <View style={styles.inputContainer}>
         <TextInput style={styles.textContainer} placeholder='Course Goal' />
         <Button title="ADD" />
       </View>
-      <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    padding: 30
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
   },
   textContainer: {
-    paddingTop: 20,
-    borderBottomColor: 'black',
-    borderBottomWidth: 1
+    padding: 10,
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1
   }
 });
