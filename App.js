fd7b45e97a06ed676e5c81aae78498a8c4c4eff6
g-hoost <gfroehling@icloud.com>
2021-08-26 12:49:50 +0200

    scrollview done

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
+import { Button, StyleSheet, Text, TextInput, View, ScrollView } from 'react-native';
 
 export default function App()
 {
@@ -26,9 +26,9 @@ export default function App()
           value={enteredGoal} />
         <Button title="ADD" onPress={addGoalHandler} />
       </View>
-      <View style={styles.textContainer2}>
-        {courseGoals.map((goal) => <View style={styles.toDoList}><Text key={goal}>{goal}</Text></View>)}
-      </View>
+      <ScrollView style={styles.textContainer2}>
+        {courseGoals.map((goal) => <View style={styles.toDoList}><Text style={styles.listText} key={goal}>{goal}</Text></View>)}
+      </ScrollView>
     </View>
   );
 }
@@ -46,16 +46,22 @@ const styles = StyleSheet.create({
     padding: 10,
     width: '80%',
     borderColor: 'black',
-    borderWidth: 1
+    borderRadius: 5,
+    borderWidth: 1,
+    fontSize: 16
   },
   toDoList: {
     padding: 10,
     marginVertical: 10,
     backgroundColor: 'lightpink',
     borderColor: 'black',
+    borderRadius: 5,
     borderWidth: 1
   },
   textContainer2: {
     marginVertical: 20
+  },
+  listText: {
+    fontSize: 16
   }
 });
